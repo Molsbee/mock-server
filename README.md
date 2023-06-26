ae4fa022aed9f7bfba9f115b76517a4ac2da89ea
molsbee <molsbee@gmail.com>
2023-06-26 09:48:55 -0500

    Working on implementing api and concept around collections.

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # mock-server
 General purpose mock server that can be configured through rest api
 
+Collection:
+    Directory: CollectionName
+        Group: File containing common routes
 
-Collection
-    Name: servers
+Example
+Collection: BMC
+    Groups:
+        servers
+        machines
 
-Api Endpoint Prefixed by Collection
-/servers
+Path: 
+    /bmc/servers/{path}
+    /bmc/machines/{path}
 
-servers
-List of Routes related to the collection
+
+Route
+```json
 {
-    Method: "GET",
-    StatusCode: 200,
-    Body: "{\"data\": \"content\"}",
-    ContentType: "application/json",
-    ResponseHeaders: [
-        "Authorization: Bearer Token",
-        "Content-Type: application/json"
-    ]
+  "method": "GET",
+  "path": "/",
+  "headers": {
+    "Authorization": "Bearer token",
+    "Content-Type": "application/json"
+  },
+  "statusCode": 200,
+  "body": "{\"some\": \"info\"}"
 }
+```
--- service/collections.go
+++ service/collections.go
@@ -1,30 +1,41 @@
 package service
 
 import (
+	"log"
 	"os"
 )
 
-func GetCollections() (collections []string) {
-	files, _ := os.ReadDir("/collections")
-	for _, file := range files {
-		collections = append(collections, file.Name())
+var collectionsDirectory string
+
+func init() {
+	currentDirectory, _ := os.Getwd()
+	collectionsDirectory = currentDirectory + "/collections"
+
+	if _, err := os.Stat(collectionsDirectory); err != nil {
+		if dirErr := os.Mkdir(collectionsDirectory, 0666); dirErr != nil {
+			log.Panicln("failed to initialize the collections directory")
+		}
 	}
-	return
+}
+
+func GetCollections() []string {
+	files, _ := os.ReadDir(collectionsDirectory)
+	collections := make([]string, len(files))
+	for i, file := range files {
+		collections[i] = file.Name()
+	}
+	return collections
 }
 
 func CreateCollection(name string) error {
-	_, err := os.Create("/collections/" + name)
+	err := os.Mkdir(collectionsDirectory+"/"+name, 0666)
 	return err
-	//return os.WriteFile("/collections/"+name, nil, 0666)
 }
 
 func DeleteCollection(name string) error {
-	if _, err := os.Open("/collections/" + name); err != nil {
-		// Handle error
-	}
-
-	if err := os.Remove("/collections/" + name); err != nil {
-		// Handle error
+	_, err := os.Stat(collectionsDirectory + "/" + name)
+	if err == nil {
+		return os.Remove(collectionsDirectory + "/" + name)
 	}
 	return nil
 }
